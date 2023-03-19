30fce4527cfa7cb2953db17fdd2946bf0fa909d6
DmitryBaranovAndreevich <baranow.dmitry@gmail.com>
2023-03-19 10:46:45 +0300

    change config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv'
-dotenv.config()
-console.log(process.env)
+require("dotenv").config();
 
 const {
   DEPLOY_USER,
@@ -9,20 +7,24 @@ const {
   DEPLOY_REF = "origin/main",
 } = process.env;
 
-export const apps = [
-  {
-    name: "mesto",
-    script: "./dist/app.js",
-  },
-];
-export const deploy = {
-  production: {
-    user: DEPLOY_USER,
-    host: DEPLOY_HOST,
-    ref: DEPLOY_REF,
-    repo: "https://github.com/DmitryBaranovAndreevich/mesto-project-plus.git",
-    path: DEPLOY_PATH,
-    "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-    "post-deploy": "npm i && npm run build",
+module.exports = {
+  apps: [
+    {
+      name: "mesto",
+      script: "./dist/app.js",
+    },
+  ],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: "https://github.com/DmitryBaranovAndreevich/mesto-project-plus.git",
+      path: DEPLOY_PATH,
+      "pre-deploy-local": `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      "post-deploy": "npm i && npm run build",
+    },
   },
 };
